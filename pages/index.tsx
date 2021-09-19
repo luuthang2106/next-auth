b8127602fe2d31082163ecfa15f0efb6aa383057
luuthang2106 <luuthang2106@gmail.com>
2021-09-19 21:17:10 +0700

    pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      Hello world!
+    <div className="container mx-auto p-6 text-sm">
+      <div className="shadow p-6">
+        <div className="flex w-full">
+          <Link href="/login">
+            <button className="px-4 py-2 ml-auto mr-4 shadow">
+              Login
+              </button>
+          </Link>
+          <Link href="/register">
+            <button className="px-4 py-2 shadow">
+              Register
+              </button>
+          </Link>
+        </div>
+        <p>Guest Page!</p>
+      </div>
     </div>
   )
 }
---
+++ pages/login.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+
+const Login: NextPage = () => {
+    return (
+        <div className="w-screen h-screen flex items-center justify-center text-sm">
+            <div className="w-11/12 md:w-96 shadow p-6">
+                <div className="flex flex-col w-full space-y-4">
+                    <p className="uppercase text-center font-bold">Login</p>
+                    <div className="flex flex-col space-y-2">
+                        <label>Username</label>
+                        <input className="h-6 px-2 shadow outline-none" type="email" />
+                    </div>
+                    <div className="flex flex-col space-y-2">
+                        <label>Password</label>
+                        <input className="h-6 px-2 shadow outline-none" type="password" />
+                    </div>
+                    <button className="uppercase w-full bg-gray-200 font-bold text-gray-500 py-3">
+                        Submit
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
